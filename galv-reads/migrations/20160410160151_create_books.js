01081071a34ef3b6b6ad532984ed7c860a5385f2
derzorngottes <amy.conchie@gmail.com>
2016-04-10 16:24:01 -0700

    Update migrations

---
+++ galv-reads/migrations/20160410160151_create_books.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('books', function (table) {
+      table.increments();
+      table.string('title');
+      table.string('genre');
+      table.text('description');
+      table.string('coverUrl');
+    }),
+    knex.schema.createTable('authors', function (table) {
+      table.increments();
+      table.string('firstName');
+      table.string('lastName');
+      table.text('bio');
+      table.string('portraitUrl');
+    }),
+    knex.schema.createTable('books_authors', function (table) {
+      table.increments();
+      table.number('bookId');
+      table.number('authorId');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('books');
+    knex.schema.dropTable('authors');
+    knex.schema.dropTable('books_authors');
+  ]);
+};
