d1c188b530c179a7fd1f837526389ead8028220b
derzorngottes <amy.conchie@gmail.com>
2016-04-12 18:11:15 -0700

    Create seed file

--- galv-reads/routes/books.js
+++ galv-reads/routes/books.js
@@ -1,3 +1,20 @@
+/* WORKFLOW:
+
+1. check seed file from current open directory & build out?
+
+2. get authors table loaded.bs.modal
+
+3. build relationship/joins table
+
+4. get promises working
+
+5. make other CRUD stuff working
+
+6. add oAuth functionality
+
+*/
+
+
 var express = require('express');
 var router = express.Router();
 var methodOverride = require('method-override');
@@ -16,8 +33,17 @@ function BooksAuthors() {
 router.use(methodOverride('_method'));
 
 router.get('/books', function(req, res, next) {
-  Books().select().then(function (records) {
-    res.render('books/books', {allBooks: records});
+
+  // Books().select().then(function (records) {
+  //   res.render('books/books', {allBooks: records});
+  // });
+
+  Books().select().then(function(records){
+    Authors().select().then(function(authors) {
+      BooksAuthors().select().then(function(howjoin) {
+        res.render('books/books', { allBooks: records, allAuthors: authors, howRel: howjoin });
+      });
+    });
   });
 });
 
